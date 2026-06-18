4cd01406636f319db12f0fcff9cf862cad626862
Justin1680 <bricenoroblesjustin@gmail.com>
2026-06-18 12:31:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gestión_de_Matrículas_Universitarias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
